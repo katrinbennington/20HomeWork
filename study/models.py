13c9f05459815202654e5c7b3af1128e6c58a91e
Katrin <katrin_bennington@mail.ru>
2024-09-03 19:21:52 +0500

    homework 24.1 with postman

---
+++ study/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.utils import timezone
+
+from config import settings
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название")
+    image = models.ImageField(
+        upload_to="courses/photo", verbose_name="Превью", help_text="Загрузите изображение")
+    description = models.CharField(max_length=100, verbose_name="Описание")
+
+    def __str__(self):
+        return f"{self.name} {self.image} {self.description}"
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+        ordering = ("name",)
+
+
+class Lesson(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название",
+        help_text="Введите название урока",
+    )
+    description = models.CharField(
+        max_length=100, verbose_name="Описание", help_text="Введите описание урока"
+    )
+    image = models.ImageField(
+        upload_to="lessons/photo", verbose_name="Превью", help_text="Загрузите изображение"
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,
+        verbose_name="Курс",
+        null=True,
+        blank=True,
+        related_name="lessons",
+    )
+    video = models.CharField(
+        max_length=100,
+        verbose_name="Ссылка на видео",
+        help_text="Укажите ссылку на видео",
+    )
+
+    def __str__(self):
+        return (
+            f"{self.name} {self.description} {self.image} {self.course} {self.video}")
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+        ordering = ("name",)
---
+++ study/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from study.apps import StudyConfig
+from rest_framework.routers import DefaultRouter
+
+from study.views import (CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView,
+                         LessonUpdateAPIView, LessonDestroyAPIView)
+
+app_name = StudyConfig.name
+
+
+router = DefaultRouter()
+router.register('сourse', CourseViewSet, basename='сourse')
+
+urlpatterns = [
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='moto-create'),
+    path('lesson/list/', LessonListAPIView.as_view(), name='moto-list'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='moto-get'),
+    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='moto-update'),
+    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='moto-delete'),
+] + router.urls
