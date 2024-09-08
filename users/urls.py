9e82fc1a79093bd85c4870d6499357e89f1b223e
Katrin <katrin_bennington@mail.ru>
2024-09-08 10:12:49 +0500

    CRUD (RUD) for Users

--- users/urls.py
+++ users/urls.py
@@ -4,14 +4,18 @@ from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import PaymentAPIViewSet, PaymentCreateAPIView, UserCreateAPIView
+from users.views import PaymentAPIViewSet, PaymentCreateAPIView, UserCreateAPIView, UserListAPIView, UserUpdateAPIView, \
+    UserDestroyAPIView
 #from users.views import UserCreateView, email_verification, PasswordResetView
 from django.urls import path
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('register/', UserCreateAPIView.as_view(), name='register'),
+    path('register/', UserCreateAPIView.as_view(), name='user-create'),
+    path('list/', UserListAPIView.as_view(), name='user-retrieve'),
+    path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='user-update'),
+    path('delete/<int:pk>/', UserDestroyAPIView.as_view(), name='user-destroy'),
     path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
     path('logout/', LogoutView.as_view(), name='logout'),
--- users/views.py
+++ users/views.py
@@ -15,7 +15,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from requests import Response
 from rest_framework import generics
 from rest_framework.filters import OrderingFilter
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.viewsets import ModelViewSet
 
 from users.forms import UserRegisterForm
@@ -32,6 +32,39 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
 class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class UserListAPIView(ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class UserDestroyAPIView(DestroyAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
