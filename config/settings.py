7ef43dec9ced209843765459fc9e35319e358733
Katrin <katrin_bennington@mail.ru>
2024-09-11 11:36:29 +0500

    add media to settings, fix unnecessary directories

--- config/settings.py
+++ config/settings.py
@@ -147,6 +147,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 AUTH_USER_MODEL = 'users.User'
 
 LOGIN_REDIRECT_URL = '/'
